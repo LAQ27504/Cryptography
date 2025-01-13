b14087d0de4f06445be7c505fb40e15928b76ffb
LAQ27504 <anhquangcsp@gmail.com>
2025-01-14 02:16:41 +0700

    t

--- lab3.py
+++ lab3.py
@@ -1,3 +1,4 @@
+from GCD import gcd
 from xor import convert_to_binary, convert_binary_back 
 inputs = []
 with open("lap_3_input.txt", "r") as f:
@@ -27,9 +28,26 @@ for i in range(len(binary_array)//8):
       number_binary.reverse()
       binary_lists.append(number_binary)
 print(binary_lists)
-a  = [convert_binary_back(i) for i in binary_lists]
-a.reverse()
-print([chr(i) for i in a])
+# a  = [convert_binary_back(i) for i in binary_lists]
+# a.reverse()
+# print([chr(i) for i in a])
 
-#ex3
+# #ex3
 
+# p = 53
+# q = 17
+# n = p * q
+# print(n)
+# phi = (p - 1)*(q - 1)
+# e = 17
+# print(phi)
+# print(gcd(e, phi))
+# d = pow(e, -1, phi)
+# print(d)
+# print(pow(428,e,n))
+# print(pow(112,d,n))
+
+# print(type(str(e)))
+a = "abc"
+
+print(a.find("d"))
